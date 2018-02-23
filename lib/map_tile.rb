4346b45b95e69695dcdff168d3a6330337d41201
yynozk <yynozk@gmail.com>
2018-02-23 14:11:13 +0900

    feat: 画面外のタイルを描画しないことで高速化

--- lib/map_tile.rb
+++ lib/map_tile.rb
@@ -9,10 +9,21 @@ class MapTile < Sprite
     super()
     self.x, self.y = x * @@tile_width, y * @@tile_height
     self.image = @@tileset[tile_index]
+
+    @in_window_range_x = -self.image.width..Window.width
+    @in_window_range_y = -self.image.height..Window.height
+  end
+
+  def update
+    self.visible = in_window?
   end
 
   def scroll(offset)
     self.x += offset[:x]
     self.y += offset[:y]
+  end
+
+  def in_window?
+    @in_window_range_x === self.x and @in_window_range_y === self.y
   end
 end
